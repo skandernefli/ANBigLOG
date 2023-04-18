ac0779c8267e7668d2ffb14d6120a676b404512c
skandernefli <skander.nefli@etudiant-isi.utm.tn>
2023-04-18 04:35:52 +0200

    datagrid

--- dashboard/src/scenes/postsList/index.jsx
+++ dashboard/src/scenes/postsList/index.jsx
@@ -1,7 +1,62 @@
+import { DataGrid } from '@mui/x-data-grid';
+import { useState, useEffect } from 'react';
+import { Box, IconButton, useTheme } from "@mui/material";
 
-
-
+import { useNavigate } from 'react-router-dom';
+import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 const PostsList =()=>{
-    return <div>managePosts</div>
+    const [data, setData] = useState([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+        fetch('http://localhost:8000/server/post/admin/')
+          .then(response => response.json())
+          .then(result => setData(result))
+          .catch(error => console.log(error));
+      }, []);
+      const columns = [
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'admin_id', headerName: 'Admin ID', width: 50 },
+        { field: 'categorie_id', headerName: 'Category ID', width: 50 },
+        { field: 'title', headerName: 'Title', width: 300 },
+        { field: 'post_id', headerName: 'Post ID', width: 100 },
+        { field: 'created_at', headerName: 'Created At', width: 150 },
+        { field: 'updated_at', headerName: 'Updated At', width: 150 },
+        {
+          field: 'actions',
+          headerName: 'Actions',
+          width: 80,
+          renderCell: (params) => (
+            <>
+              <IconButton aria-label="delete" onClick={() => handleDelete(params.row.id)}>
+                <RemoveCircleOutlineOutlinedIcon />
+              </IconButton>
+              <IconButton aria-label="edit" onClick={() => handleEdit(params.row.id)}>
+                <CreateOutlinedIcon />
+              </IconButton>
+            </>
+          ),
+        },
+      ];
+      const handleDelete = (id) => {
+        fetch(`your_backend_api_url/${id}`, { method: 'DELETE' })
+        .then(() => {
+            const updatedData = data.filter((item) => item.id !== id);
+            setData(updatedData);
+          })
+          .catch(error => console.log(error));
+        };
+
+        const handleEdit = (id) => {
+        navigate('/edit/${id}');
+        };
+
+    return (<div style={{ height: '100%', width: '100%',padding:'5%' }}>
+    <DataGrid
+          columns={columns}
+          rows={data}
+          autoHeight={true}
+        />
+    </div>)
 }
 export default PostsList;
