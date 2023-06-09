85829071e22a337d5f7b3da4ed5f6727a9a153d9
skandernefli <skander.nefli@etudiant-isi.utm.tn>
2023-06-09 23:30:10 +0200

    https

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
-const http =require('http');
+const https =require('https');
+const fs=require('fs');
+const options={
+    key:fs.readFileSync('ssl/key.pem'),
+    cert:fs.readFileSync('ssl/cert.pem')
+}
 const App=require('./App');
 const port=8000;
 
-const server=http.createServer(App);
+const server=https.createServer(options,App);
 server.listen(port)
