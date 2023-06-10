cfcde9f73ad5601f22601d7cb451872855f225df
skandernefli <skander.nefli@etudiant-isi.utm.tn>
2023-06-11 00:50:58 +0200

    https

--- client/src/components/Home/allPosts.vue
+++ client/src/components/Home/allPosts.vue
@@ -177,7 +177,7 @@ export default {
     },
     async fetchPosts() {
       try {
-        const response = await fetch("https://3.145.167.18:8000/server/post/client/");
+        const response = await fetch("https://18.218.162.154/server/post/client/");
         this.posts = await response.json();
       } catch (error) {
         console.error(error);
